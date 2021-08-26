0d5d39b1eae8873a44a75d5ab472480765f99779
AureRigo <aurelienrigolet3@gmail.com>
2021-08-26 14:16:51 +0200

    my javascript's page has now some personalised CSS style and some JS attributes

---
+++ main.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("but");
+let text = document.getElementById("text");
+
+button.onclick = function () {
+    text.innerHTML = "Cette année, grace à Javascript, j'ai appris les bases de l'algorythmie. J'ai pu déclarer mes premières variables, mes premières fonctions. Par la suite, nous avons vu les objets Javascripts. Nous avons également vu commen uttiliser l'API Audio de Javascript ou comment créer des jeux en Javascript à l'aide de P5 ou bien encore Node JS.";
+}
